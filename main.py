6c315ef41541a597843bc1bb0065a196484f35ae
229935 <229935@edu.p.lodz.pl>
2020-11-29 04:04:01 +0100

    Add models f(x) = ax and f(x) = ax + b.

--- main.py
+++ main.py
@@ -10,6 +10,57 @@ data2 = pd.read_csv("data2.csv", names = ["X", "Y"])
 data3 = pd.read_csv("data3.csv", names = ["X1", "X2", "Y"])
 data4 = pd.read_csv("data4.csv", names = ["X1", "X2", "Y"])
 
+# variance
+
+def variance(dt):
+    meanx = dt.X.mean()
+    var = 0
+
+    for x in range(100):
+        var += ((dt.X - meanx) * (dt.X - meanx))
+
+    return var.mean()
+
+# covariance
+
+def covariance(dt):
+    meanx = dt.X.mean()
+    meany = dt.Y.mean()
+    cov = 0
+
+    for x in range(100):
+        cov += ((dt.X - meanx) * (dt.Y - meany))
+
+    return cov.mean()
+
+# model 1: f(x) = a * X
+
+def model1(dt):
+    meanx = dt.X.mean()
+    meany = dt.Y.mean()
+    a = meany / meanx
+    print('f(x) = ' + str(round(a, 2)) + " * X")
+
+# model 2: f(x) = a * X + b
+
+def model2(dt):
+    meanx = dt.X.mean()
+    meany = dt.Y.mean()
+    a = covariance(dt) / variance(dt)
+    b = meany - a * meanx
+    print('f(x) = ' + str(round(a, 2)) + " * X + " + str(round(b, 2)))
+
+print("\n--- ZESTAW DANYCH 1 ---")
+
+model1(data1)
+model2(data1)
+
+print("\n--- ZESTAW DANYCH 2 ---")
+
+model1(data2)
+model2(data2)
+
+
 # function plotting a dataframe
 
 def twoDimensionGraph(dt, n):
@@ -18,8 +69,3 @@ def twoDimensionGraph(dt, n):
     dt.plot(x = "X", y = "Y", color = 'red', marker = 'o', linewidth = '0', markersize = '2')
     plt.savefig(fname = n.__str__())
     plt.close(n)
-
-# calling a plotting function for dataframes 1 and 2
-
-twoDimensionGraph(data1, 1)
-twoDimensionGraph(data2, 2)
